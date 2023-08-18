354849310346148ff18a7c44c67acf8571cc0522
trung696 <trungbvt99@gmail.com>
2023-08-18 17:57:41 +0700

    đăng nhập

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -20,7 +20,7 @@
           </div>
           <div class="col-3 sign">
             <div class="" style="float: right; margin-right: 10%; margin-top: 3%; color:#FFB72D">
-              <div class="d-sm-inline-block d-none "> Xin chào , <a href="" style="color: #FFB72D; text-decoration: none;">&nbsp; Bách Nguyễn</a></div>
+              <div class="d-sm-inline-block d-none "><a href="/dangnhap" style="color: #FFB72D; text-decoration: none;"> Đăng nhập ,</a> <a href="/dangky" style="color: #FFB72D; text-decoration: none;">&nbsp; Đăng ký</a></div>
               <img src="/images/avatar.png" />
             </div>
           </div>
@@ -38,9 +38,7 @@
             <img src="/images/vfantasy_mobile.png" class="small-vfantasy d-inline d-md-none"/>
 
             <p class="fanta d-none d-xl-block mb-3">VFANTASY</p>
-            <select class="form-select form-rounded" id="team-select">
-              <option value="">GoonerBBB</option>
-            </select>
+
           </div>
 
       <div class=" d-flex vfatasy-menu pill">
@@ -50,25 +48,57 @@
 
       </div>
     </div>
-    <div class="container d-flex justify-content-center">
+    <div class="container d-flex justify-content-center ">
         <div class="login-form d-block">
             <div class="email-form">
-                <div class="login-label">Tên đăng nhập</div>
-                <input type="email" class="form-control-lg" id="exampleFormControlInput1" placeholder="name@example.com">
+                <div class="login-label mb-3">TÊN ĐĂNG NHẬP</div>
+                <input type="email" class="form-control-lg rounded-pill w-100 ip-form" id="exampleFormControlInput1" placeholder="Email hoặc số điện thoại của bạn">
 
             </div>
-            <div class="email-form">
-                <div class="login-label">Mật khẩu</div>
-                <input type="password" class="form-control-lg" id="exampleFormControlInput1" placeholder="name@example.com">
+            <div class="email-form mt-3">
+                <div class="login-label mb-3">MẬT KHẨU</div>
+                <input type="password" class="form-control-lg rounded-pill w-100 ip-form" id="exampleFormControlInput1" placeholder="Mật khẩu ít nhất 8 kí tự">
 
             </div>
-            {{-- <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                <input type="email" class="form-control-lg" id="exampleFormControlInput1" placeholder="name@example.com">
-              </div> --}}
+            <button type="button" class="bt-signin mt-4 w-100">ĐĂNG NHẬP</button>
+            <div class="forget mt-4">
+                <a href="#">Quên mật khẩu</a>
+            </div>
+            <p class="mt-5 mb-3 text-center">Hoặc</p>
+            {{-- <a href="" class=" ons-sign d-inline-block w-100 " style="padding-left: 85px">
+                <img src="images/partner.png" alt="" class="mx-auto d-inline-block align-middle">
+                <div class="d-inline-block align-middle">
+                <p class="ons-plus">Đăng nhập bằng</p>
+                <p class="ons-plus">tài khoản onplus</p>
+                </div>
+            </a> --}}
+            <button class="w-100 bt-8px">
+                <img src="images/partner.png" alt="" class="mx-auto d-inline-block align-middle">
+                <div class="d-inline-block align-middle">
+                <p class="ons-plus">Đăng nhập bằng</p>
+                <p class="ons-plus">tài khoản onplus</p>
+                </div>
+            </button>
+            <div class="d-flex gap-2 mt-2 mb-3">
+                <button class="w-50 p-2 w-signin bt-8px">
+                    <img src="images/gg.png" alt="" class="d-inline-block align-middle" style="margin-right: 10px">
+                    Google</button>
+                <button class="w-50 p-2 w-signin bt-8px">
+                    <img src="images/fb.png" alt="" class=" d-inline-block align-middle" style="margin-right: 10px">
+                    Facebook</button>
+            </div>
+
 
         </div>
 
     </div>
+    <hr>
+    <footer class="container" style="gap: 31px">
+        <div class="d-flex">
+            <p class="me-auto align-self-center" style="font-size: 14px;font-weight: 400;">© 2023 All Rights Reserved.</p>
+            <a href="#vtop" class="align-self-center m-3" style="color: black; text-decoration: none;font-size: 14px;font-weight: 400;">BACK TO TOP</a>
+            <img src="/images/vfantasy.png" alt="" class="" style="height: 57px">
+        </div>
+    </footer>
 </body>
 </html>
